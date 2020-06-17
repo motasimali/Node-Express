53e2d3b36cddcc363bbc6237ab37129000919865
Motasim Ali Bhatti <bitf13a011@pucit.edu.pk>
2020-06-17 17:00:38 +0500

    Minimal Express Server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =  require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+// next is used to invoke additional middlewares
+app.use((req,res,next)=>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<html><body><h1>This is the Express server</h1></body></html>')
+});
+
+const server = http.createServer(app);
+
+server.listen(port,hostname,()=>{
+    console.log(`Server is running at http://${hostname}:${port}`)
+})
