42a878e5a55707d12f66bf7735cd7f223f8a57a6
Motasim Ali Bhatti <bitf13a011@pucit.edu.pk>
2020-06-17 18:30:54 +0500

    Assignment 1, promoRouter, leaderRouter

--- index.js
+++ index.js
@@ -13,25 +13,11 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname+'/public'));
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 app.use('/dishes',dishRouter);
-
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end('Will send details of the dish: '+ req.params.dishId+ ' to you');
-});
-
-app.post('/dishes/:dishId',(req,res,next)=>{
-    res.statusCode = 403;
-    res.end('PUT operation not supported on '+req.url + ' ' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId',(req,res,next)=>{
-    res.write('Updating the dish: '+ req.params.dishId+ '\n');
-    res.end('Will update the dish: '+ req.body.name+ ' with details: '+req.body.description)
-});
-
-app.delete('/dishes/:dishId',(req,res,next)=>{
-    res.end('Delteting dish: '+ req.params.dishId);
-});
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // next is used to invoke additional middlewares
 app.use((req,res,next)=>{
